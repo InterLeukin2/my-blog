f4b21a4c647a9ade8cfe30edd82858fb1f5d69e3
lnterleukin2 <interleukin2@proton.me>
2024-12-23 00:36:54 +0800

    gaifengl2

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,15 +1,19 @@
 import { notFound } from 'next/navigation'
 import PostContent from './PostContent'
 
+interface PageProps {
+  params: {
+    slug: string;
+  };
+}
+
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
 
-export default async function PostPage({ params }: { params: { slug: string } }) {
-  const resolvedParams = await Promise.resolve(params)
-  
-  if (resolvedParams.slug === 'template') {
+export default async function PostPage({ params }: PageProps) {
+  if (params.slug === 'template') {
     notFound()
   }
   
-  return <PostContent slug={resolvedParams.slug} />
+  return <PostContent slug={params.slug} />
 } 
