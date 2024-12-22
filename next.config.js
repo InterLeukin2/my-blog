02a98aba912130767788f99b9826078acb41d60c
lnterleukin2 <interleukin2@proton.me>
2024-12-23 01:34:48 +0800

    15

--- next.config.js
+++ next.config.js
@@ -1,29 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // 基础配置
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-  
-  // 图片配置
+const config = {
+  output: 'export',
   images: {
     unoptimized: true,
-    domains: ['localhost'],
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3000',
-        pathname: '/**',
-      },
-    ],
   },
-
-  // 添加日志
-  onDemandEntries: {
-    // 开发模式的页面缓存时间
-    maxInactiveAge: 25 * 1000,
-    // 同时缓存的页面数
-    pagesBufferLength: 2,
-  },
+  basePath: process.env.NODE_ENV === 'production' ? '/my-blog' : '',
 }
 
-module.exports = nextConfig 
+module.exports = config 
