7320018a41f3b8a1c70b68d7857dfbe025b81d5d
lnterleukin2 <interleukin2@proton.me>
2024-12-23 04:08:08 +0800

    26

--- next.config.js
+++ next.config.js
@@ -1,19 +1,13 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const config = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/my-blog' : '',
-  experimental: {
-    turbo: {},
-  },
-  webpack(config) {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
-    return config;
-  },
-}
-
-module.exports = config;
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+    basePath: process.env.NODE_ENV === 'production' ? '/my-blog' : '',
+    experimental: {
+      turbo: {},  // 启用 Turbo 配置，设置为空对象
+    },
+  };
+  
+  module.exports = config;
