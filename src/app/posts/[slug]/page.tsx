abc7e6c9094c3a77ea33229d24b116fd076ac608
lnterleukin2 <interleukin2@proton.me>
2024-12-23 01:24:10 +0800

    13

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -2,21 +2,17 @@ import { notFound } from 'next/navigation'
 import PostContent from './PostContent'
 import { Metadata } from 'next'
 
-export async function generateMetadata({
-  params,
-}: {
+type Props = {
   params: { slug: string }
-}): Promise<Metadata> {
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
   return {
     title: `Post - ${params.slug}`,
   }
 }
 
-export default async function PostPage({
-  params,
-}: {
-  params: { slug: string }
-}) {
+export default async function PostPage({ params }: Props) {
   if (params.slug === 'template') {
     notFound()
   }
