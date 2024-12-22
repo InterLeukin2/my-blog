a821fee6185af605dd33a68509e4394828a5ef67
lnterleukin2 <interleukin2@proton.me>
2024-12-23 00:38:41 +0800

    gaifengl3

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,16 +1,21 @@
 import { notFound } from 'next/navigation'
 import PostContent from './PostContent'
 
-interface PageProps {
-  params: {
-    slug: string;
-  };
+// 定义参数类型
+type Params = {
+  slug: string;
+}
+
+// 定义页面属性类型
+type Props = {
+  params: Params;
+  searchParams: { [key: string]: string | string[] | undefined };
 }
 
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
 
-export default async function PostPage({ params }: PageProps) {
+export default async function PostPage({ params, searchParams }: Props) {
   if (params.slug === 'template') {
     notFound()
   }
