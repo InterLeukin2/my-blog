6b8a3745b7afe135b4c134d217c20b1f947e4c26
lnterleukin2 <interleukin2@proton.me>
2024-12-23 00:42:02 +0800

    gaifengl5

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,20 +1,15 @@
 import { notFound } from 'next/navigation'
 import PostContent from './PostContent'
 
-// 定义参数类型
-type Params = {
-  slug: string;
-}
-
-// 定义页面属性类型
-type Props = {
-  params: Params;
-}
-
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
 
-export default async function PostPage({ params }: Props) {
+// 使用 Next.js 的内置类型
+export default async function PostPage({
+  params,
+}: {
+  params: { slug: string }
+}) {
   if (params.slug === 'template') {
     notFound()
   }
