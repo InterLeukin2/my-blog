39046dc002bdf52e01bc28c5035e73424e22f242
lnterleukin2 <interleukin2@proton.me>
2024-12-23 04:15:30 +0800

    27

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import styles from './page.module.css'
-import PostList from '@/app/posts/postList'
+//import PostList from '@/app/posts/postList'
 import Image from 'next/image'
 import { siteConfig } from '@/config/site'
 import ThemeToggle from '@/components/ThemeToggle'
